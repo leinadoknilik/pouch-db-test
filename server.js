678dd69382a32872715ec2c75f2d3e1b37652b4b
Kilinkó Dániel <daniel.kilinko@heliasolution.com>
2016-09-20 12:31:08 +0200

    auth 8

--- server.js
+++ server.js
@@ -169,39 +169,38 @@ var apiRoutes = express.Router();
 // http://localhost:8080/api/authenticate
 apiRoutes.post('/authenticate', function(req, res) {
 
-
-	// find the user
   User.findOne({
   		name: req.body.name
   	}, function(err, user) {
-		if (err) throw err;
 
-		if (!user) {
-			res.json({ success: false, message: 'Authentication failed. User not found.' });
-		} else if (user) {
+  		if (err) throw err;
 
-			// check if password matches
-			if (user.password != user.body.password) {
-				res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-			} else {
+  		if (!user) {
+  			res.json({ success: false, message: 'Authentication failed. User not found.' });
+  		} else if (user) {
 
-				// if user is found and password is right
-				// create a token
-				var token = jwt.sign(user, app.get('superSecret'), {
-					expiresIn: 86400 // expires in 24 hours
-				});
+  			// check if password matches
+  			if (user.password != req.body.password) {
+  				res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+  			} else {
 
-				res.json({
-					success: true,
-					message: 'Enjoy your token!',
-					token: token
-				});
-			}
+  				// if user is found and password is right
+  				// create a token
+  				var token = jwt.sign(user,'superSecret', {
+  					expiresIn: 86400 // expires in 24 hours
+  				});
 
-		}
+  				res.json({
+  					success: true,
+  					message: 'Enjoy your token!',
+  					token: token
+  				});
+  			}
 
-	});
-});
+  		}
+
+  	});
+  });
 
 // ---------------------------------------------------------
 // route middleware to authenticate and check token
