51c20b6a051d20777a636b89ed4b1224ac2d1c29
Kilinkó Dániel <daniel.kilinko@heliasolution.com>
2016-09-21 19:19:11 +0200

    parse

--- server.js
+++ server.js
@@ -185,8 +185,8 @@ apiRoutes.get('/expenses', function(req, res) {
 });
 
 apiRoutes.get('/expenses-by-time', function(req, res) {
-  console.log(new Date(req.params.year,req.params.month));
-	Expense.find({"date": {"$gte": new Date(req.params.year,req.params.month)}}, function(err, expenses) {
+  console.log(new Date(parseInt(req.params.year),parseInt(req.params.month)));
+	Expense.find({"date": {"$gte": new Date(parseInt(req.params.year),parseInt(req.params.month)}}, function(err, expenses) {
 		res.json(expenses);
 	});
 });
